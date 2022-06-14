1f4a78c9ab8dfc30cf212d17fc0328ab2d84b7f4
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2022-06-14 20:51:23 +0700

    Task C

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -68,7 +68,7 @@ function HomePage() {
 
   return (
     <>
-      <h1>Task B</h1>
+      <h1>Task C</h1>
       <Card size="small" title="Tạo sản phẩm mới">
         <Form
           name="Create Product"
